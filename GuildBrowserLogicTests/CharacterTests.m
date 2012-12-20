63f5a966f88e9302ede79985e4de3b8449ba9f82
xuwaer <xuwaer@gmail.com>
2012-12-20 15:31:05 +0800

    fix unit test case bug

--- GuildBrowserLogicTests/CharacterTests.m
+++ GuildBrowserLogicTests/CharacterTests.m
@@ -13,10 +13,7 @@
 
 @implementation CharacterTests
 {
-    NSDictionary *_characterDetailJson;
-    
-    Character *_testGuy;
-}
+    NSDictionary *_characterDetailJson;}
 
 -(void)setUp
 {
@@ -47,6 +44,9 @@
 
 -(void)testCreateCharacterFromDetailJsonProps
 {
+    Character *_testGuy = [[Character alloc] initWithCharacterDetailData:_characterDetailJson];
+    STAssertNotNil(_testGuy, nil);
+    
     STAssertEqualObjects(_testGuy.thumbnail, @"borean-tundra/171/40508075-avatar.jpg", @"thumbnail url is wrong");
     STAssertEqualObjects(_testGuy.name, @"Hagrel", @"name is wrong");
     STAssertEqualObjects(_testGuy.battleGroup, @"Emberstorm", @"battlegroup is wrong");
@@ -63,6 +63,9 @@
 
 -(void)testCreateCharacterFromDetailJsonValidateItems
 {
+    Character *_testGuy = [[Character alloc] initWithCharacterDetailData:_characterDetailJson];
+    STAssertNotNil(_testGuy, nil);
+    
     STAssertEqualObjects(_testGuy.neckItem.name,@"Stoneheart Choker", @"name is wrong");
     STAssertEqualObjects(_testGuy.wristItem.name,@"Vicious Pyrium Bracers", @"name is wrong");
     STAssertEqualObjects(_testGuy.waistItem.name,@"Girdle of the Queen's Champion", @"name is wrong");
